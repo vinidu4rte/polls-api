02d7430d87da9b95eb8805ed1b1ab405f1bca047
Vinicius Duarte <viniaduarrte@gmail.com>
2023-08-21 21:29:27 -0300

    feat: ensure SignUpController return error in body and status 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -16,4 +16,20 @@ describe('SignUpController', () => {
     expect(statusCode).toBe(400)
     expect(body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Ensure return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      name: 'any_name',
+      password: 'any_password',
+      passwordConfirmation: 'any_password_confirmation'
+    }
+
+    const response = sut.handle(httpRequest)
+    const { statusCode, body } = response
+
+    expect(statusCode).toBe(400)
+    expect(body).toEqual(new Error('Missing param: email'))
+  })
 })
